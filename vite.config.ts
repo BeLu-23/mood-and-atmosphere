d6774e92969d32230924076ffae991544b84537f
Benjamin Lutz <benjamin.lutz936@gmail.com>
2024-06-29 15:19:08 +0200

    1. commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // TODO change this to '/' when published on all-inkl.com
+  base: '/mood-and-atmosphere/',
 })
